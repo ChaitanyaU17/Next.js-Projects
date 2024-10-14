f2542f49ec92f908d3dc4f9d4b68a28bc49ad074
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-10-14 14:49:55 +0530

    adding nested routes to the about page

--- nextjs-task/pages/about/about.js
+++ nextjs-task/pages/about/about.js
@@ -1,4 +1,4 @@
-// our-domail.com /
+// our-domain.com /
 
 function AboutPage() {
     return <h1>The About us page</h1>
---
+++ nextjs-task/pages/about/developer.js
@@ -0,0 +1,7 @@
+// our-domain.com/aboutus/developer
+
+function developerPage() {
+    return <h1>Hello Chaitanya</h1>
+};
+
+export default developerPage;
