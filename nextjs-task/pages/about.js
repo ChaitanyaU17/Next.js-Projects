2f54411265b371c46455b56911559558e41a52f1
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-10-14 19:51:28 +0530

    Dynamic page routes

---
+++ nextjs-task/pages/about.js
@@ -0,0 +1,7 @@
+// our-domain.com /
+
+function AboutPage() {
+    return <h1>The About us page</h1>
+};
+
+export default AboutPage;
---
+++ nextjs-task/pages/aboutus/[id].js
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+
+const details = [
+    { id: '1', name: 'Yash', role: 'Senior Developer' },
+    { id: '2', name: 'Vaibhav', role: 'Backend Developer'},
+    { id: '3', name: 'Suresh', role: 'Frontend Developer'}
+];
+
+function DeveloperPage() {
+    const router = useRouter();
+    const {id} = router.query;
+
+    const developer = details.find((dev) => dev.id === id);
+
+    if (!developer) {
+        return <h1>Developer doesn't exist</h1>
+    }
+
+    return (
+        <div>
+            <h1>{developer.name}</h1>
+            <h2>{developer.role}</h2>
+        </div>
+    )
+};
+
+export default DeveloperPage;
